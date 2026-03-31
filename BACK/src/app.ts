f02c670cfbc6843aa22e4d52ad6d1b03b31484f8
Yevestevez <yevestevez.cg@gmail.com>
2026-03-31 12:17:08 +0200

    Add server static generation SSG path to public folder in Express app

--- BACK/src/app.ts
+++ BACK/src/app.ts
@@ -3,6 +3,8 @@ import express from 'express';
 export const app = express();
 console.log('Express app created');
 
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
     res.send('Express app working');
 });
