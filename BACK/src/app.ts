a3669617c0e0ba158a982c0c71b3ddd59d76ad62
Yevestevez <yevestevez.cg@gmail.com>
2026-03-31 13:13:44 +0200

    Add api/potatoes path

--- BACK/src/app.ts
+++ BACK/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import potatoesRouter from './router/potatoes-router.ts';
 
 export const app = express();
 console.log('Express app created');
 
+app.use(express.json());
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send('Express app working');
 });
+
+app.use('/api/potatoes', potatoesRouter());
