46203ee604f45a2ca2865718fd6ef83bfa987c68
Yevestevez <yevestevez.cg@gmail.com>
2026-03-31 15:11:27 +0200

    Update potato schema using Zod

--- BACK/src/schemas/potato.ts
+++ BACK/src/schemas/potato.ts
@@ -1,9 +1,23 @@
-import type { Farmer } from './farmer.ts';
+import * as z from 'zod';
 
-export interface Potato {
-    id: string;
-    weight: number;
-    price: number;
-    color: string;
-    owner?: Farmer;
-}
+export const PotatoSchema = z.object({
+    id: z.uuid(),
+    weight: z.coerce.number(),
+    price: z.coerce.number(),
+    color: z.string().optional(),
+    owner: z.string().optional(),
+});
+
+export const PotatoSchemaDTO = PotatoSchema.omit({ id: true });
+
+export type Potato = z.infer<typeof PotatoSchema>;
+export type PotatoDTO = z.infer<typeof PotatoSchemaDTO>;
+export type PotatoUpdateDTO = Partial<PotatoDTO>;
+
+// export interface Potato {
+//     id: string;
+//     weight: number;
+//     price: number;
+//     color?: string;
+//     owner?: string;
+// }
