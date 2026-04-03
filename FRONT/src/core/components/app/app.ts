7f270c27d9563f164856e0abdbb6074e62cc6199
Yevestevez <yevestevez.cg@gmail.com>
2026-04-03 12:05:19 +0200

    Configure Front basic project

---
+++ FRONT/src/core/components/app/app.ts
@@ -0,0 +1,35 @@
+//import { routes } from '../../router/router';
+//import { Footer } from '../footer-wc/footer.wc';
+//import { Header } from '../header/header.wc';
+//import { Menu } from '../menu/menu.wc';
+//import './app.css';
+
+export class App extends HTMLElement {
+    static #selector = 'app-root';
+    static render() {
+        customElements.define(App.#selector, App);
+        //Header.render();
+        //Menu.render(routes);
+        //Footer.render();
+    }
+
+    #template!: string;
+
+    constructor() {
+        super();
+        this.#setTemplate();
+        this.#setElement();
+    }
+
+    #setTemplate() {
+        this.#template = /*html*/ `
+            <app-header></app-header>
+            <main></main>
+            <app-footer></app-footer>
+        `;
+    }
+
+    #setElement() {
+        this.innerHTML = this.#template;
+    }
+}
